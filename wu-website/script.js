1ad831cf2d386d28c79d7f46701fad686058e930
MastaWu <GreatShamanGT@gmail.com>
2014-11-03 19:01:54 -0500

    Added js and css files

---
+++ wu-website/script.js
@@ -0,0 +1,40 @@
+﻿
+//jQuery code
+
+jQuery.noConflict();
+
+(function ($) {
+
+    $(function () {
+
+        $(document).ready(function () {
+
+            $('#contact').click(function () {
+
+                $('#contact-info').slideToggle('slow');
+
+            });
+
+        });
+
+        $(document).ready(function () {
+
+            $('#project').click(function () {
+
+                $('#project-info').slideToggle('slow');
+
+            });
+
+        });
+
+    });
+
+})(jQuery);
+
+//non-jQuery code
+
+function test() {
+
+    console.log(document);
+
+}
