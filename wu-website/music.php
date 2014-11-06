3230334a600c38f0a040189c61bfc538928e0256
MastaWu <GreatShamanGT@gmail.com>
2014-11-06 14:17:46 -0500

    added extra upload

--- wu-website/music.php
+++ wu-website/music.php
@@ -7,6 +7,8 @@
 <form action="upload.php" method="post" enctype="multipart/form-data">
 
     Upload your music: <input type="file" name="musicFile" /><br />
+    Name of song: <input type="text" name="songName" /><br />
+    Band name: <input type="text" name="bandName" /><br />
     <input type="submit" value="Upload Music File" />
 
 </form>
---
+++ wu-website/upload.php
@@ -0,0 +1,9 @@
+<?php
+
+$musicFile_php = $_FILES['musicFile'];
+$songName_php = $_POST['songName'];
+$bandName_php = $_POST['bandName'];
+
+echo $songName_php . "</br>" . $bandName_php;
+
+?>
