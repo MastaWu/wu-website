47d4e5dc11c3ebd6171af9c26a7b05a62e6eef52
MastaWu <GreatShamanGT@gmail.com>
2014-11-07 04:18:10 -0500

    Fully functional file upload
    Fully functional file upload

--- wu-website/addMusic.php
+++ wu-website/addMusic.php
@@ -1,22 +1,40 @@
 <?php
 
+if(!empty($_POST['submit'])){
 
-
-if(isset($_REQUEST['submit'])){
-
-    $songName_php = $_REQUEST('songName');
-    $albumName_php = $_REQUEST('albumName');
-    $bandName_php = $_REQUEST('bandName');
-    $musicFile_php = $_FILES['musicFile']['songName'];
-
+    echo "during";
     
+    $songName_php = $_POST['songName'];
+    $albumName_php = $_POST['albumName'];
+    $artistName_php = $_POST['artistName'];
+    $musicFile_php = $_FILES['musicFile']['name'];
+    $target_file = 'uploadedMusic/' . basename($_FILES['musicFile']['name']);
+    $source_file = $_FILES['musicFile']['tmp_name'];
     
-    require_once('/mysqli_connect.php');
+    if(file_exists($target_file)){
+        
+        echo "Sorry, this file already exists";
+        
+    } else {
+        
+        if(move_uploaded_file($source_file, $target_file)){
+            
+            echo "It has been uploaded";
+            
+        } else {
+            
+            echo "Upload failed";
+            
+        }
+        
+    }
+    
+    require_once('\mysqli_connect.php');
 
-    echo $songName_php . " " . $albumName_php . " " . $bandName_php;
-   
-    $addMusicQuery = mysql_query("INSERT INTO music (song_Name, music_Upload, album_Name, artist_Name) VALUES ('$songName_php', '$musicFile_php', '$albumName_php', '$bandName_php')");
+    echo $songName_php . " " . $albumName_php . " " . $artistName_php;
+    
+    $addMusicQuery = mysqli_query($database_connection, "INSERT INTO music (song_Name, music_Upload, album_Name, artist_Name) VALUES ('$songName_php', '$musicFile_php', '$albumName_php', '$artistName_php')");
 
-};
+}
 
 ?>
--- wu-website/mysqli_connect.php
+++ wu-website/mysqli_connect.php
@@ -3,7 +3,7 @@
 define ('DB_USERNAME', 'musicUpload');
 define ('DB_PASSWORD', 'password');
 define ('DB_HOST', 'localhost');
-define ('DB_NAME', testdb2);
+define ('DB_NAME', 'testdb2');
 
 $database_connection = @mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME) or 
     die('Your connection has failed. ' . mysqli_connect_error());
