f8e33273789c9b5de038159925b6094f268d3d4e
MastaWu <GreatShamanGT@gmail.com>
2014-11-08 14:06:01 -0500

    updated files

--- wu-website/music.php
+++ wu-website/music.php
@@ -1,9 +1,17 @@
-<form action="addMusic.php" method="post" enctype="multipart/form-data">
+<html>
 
-    Upload your music: <input type="file" name="musicFile" /><br />
-    <p>Name of song: <input type="text" name="songName" /></p>
-    <p>Name of Album: <input type="text" name="albumName" /></p>
-    <p>Band name: <input type="text" name="bandName" /></p>
-    <input type="submit" value="submit" />
+    <body>
 
-</form>
+        <form action="addMusic.php" method="post" enctype="multipart/form-data">
+
+            Upload your music: <input type="file" name="musicFile" /><br />
+            <p>Name of song: <input type="text" name="songName" /></p>
+            <p>Name of Album: <input type="text" name="albumName" /></p>
+            <p>Artist name: <input type="text" name="artistName" /></p>
+            <input type="submit" value="submit" name="submit"/>
+
+        </form>
+
+    </body>
+
+</html>
--- wu-website/mysqli_connect.php
+++ wu-website/mysqli_connect.php
@@ -1,9 +1,9 @@
 <?php
 
-define ('DB_USERNAME', 'musicUpload');
-define ('DB_PASSWORD', 'password');
-define ('DB_HOST', 'localhost');
-define ('DB_NAME', 'testdb2');
+define ('DB_USERNAME', 'stephgv0_music');
+define ('DB_PASSWORD', 'iloveRoxann#7');
+define ('DB_HOST', 'stephen-wu.com');
+define ('DB_NAME', 'stephgv0_testdb');
 
 $database_connection = @mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME) or 
     die('Your connection has failed. ' . mysqli_connect_error());
