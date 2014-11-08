d3ce88d69f366b1cc57dc99540cc3c39f0fba394
MastaWu <GreatShamanGT@gmail.com>
2014-11-08 17:57:53 -0500

    Form format

--- wu-website/music.php
+++ wu-website/music.php
@@ -1,24 +1,64 @@
-<?php
+<!DOCTYPE html>
 
-$title = "Home";
+<html>
 
-include ("header.php");
+    <head>
 
-?>
+        <script type="text/javascript" src="jQuery/jquery-2.1.1.min.js"></script>
 
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" />
 
-        <form action="addMusic.php" method="post" enctype="multipart/form-data">
+        <link href="index.css" rel="stylesheet" />
 
-            Upload your music: <input type="file" name="musicFile" /><br />
-            <p>Name of song: <input type="text" name="songName" /></p>
-            <p>Name of Album: <input type="text" name="albumName" /></p>
-            <p>Artist name: <input type="text" name="artistName" /></p>
-            <input type="submit" value="submit" name="submit"/>
+        <script type="text/javascript" src="musicScript.js"></script>
 
-        </form>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
 
-<?php
+        <title>New Day New Song Music Upload</title>
 
-include ("footer.php");
+    </head>
 
-?>
+    <body>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4 col-sm-offset-3 col-md-4 col-md-offset-3 col-lg-6 col-lg-offset-3 musicUploadForm">
+        
+                        <form role="form" action="addMusic.php" method="post" enctype="multipart/form-data">
+
+                        <div class="form-group">
+                            <label for="musicUploadInput">Upload your music</label>
+                            <input type="file" class="form-control" id="musicFile" name="musicFile" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="nameOfSongInput">Name of song:</label>
+                            <input type="text" class="form-control" id="songName" name="songName" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="genreTypeInput">Genre:</label>
+                            <input type="text" class="form-control" id="genreType" name="genreType" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="nameOfAlbumInput">Name of Album:</label>
+                            <input type="text" class="form-control" id="albumName" name="albumName" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="nameOfArtistInput">Name of Artist:</label>
+                            <input type="text" class="form-control" id="artistName" name="artistName" />
+                        </div>
+
+                        <button type="submit" class="btn btn-default" value="submit" name="submit">Submit
+                        </button>
+
+                        </form>
+                </div>
+            </div>
+        </div>
+
+    </body>
+
+</html>
---
+++ wu-website/musicScript.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+
+    
+
+    $(window).resize(function () {
+
+        $('.musicUploadForm').css({
+            top: ($(window).height() - $('.musicUploadForm').outerHeight()) / 2,
+            bottom: ($(window).height() - $('.musicUploadForm').outerHeight()) / 2
+        });
+
+    });
+
+    $(window).resize();
+
+});
